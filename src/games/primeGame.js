51a566f02711e5f130b91c9c91451212ce24a1a1
xenia513 <xenia513@yandex.ru>
2025-08-15 18:27:23 +0400

    fixed prime game

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,11 +3,11 @@ import playGame, { roundsCount } from '../index.js'
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (n) => { // функция-предикат вычисляет, является ли число простым
-  if (n < 2 || n === 2 || n % 2 === 0) {
+  if (n < 2) {
     return false
   }
   else {
-    for (let i = 3; i <= Math.sqrt(n); i += 1) {
+    for (let i = 2; i <= Math.sqrt(n); i += 1) {
       if (n % i === 0) {
         return false
       }
